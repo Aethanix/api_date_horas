96fdb6d8e5fcaf71bcd65aa72ef64340f8c2c9a9
Aethanix <aethanix@gmail.com>
2025-05-10 10:01:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ app.get('/', (req, res) => {
   const now = new Date();
 
   const dia = String(now.getDate()).padStart(2, '0');
-  const mes = String(now.getMonth() + 1).padStart(2, '0'); // Janeiro é 0
+  const mes = String(now.getMonth() + 1).padStart(2, '0'); // Janeiro = 0
   const ano = now.getFullYear();
 
   const dataFormatada = `${dia}-${mes}-${ano}`;
 
-  res.send(dataFormatada);
+  res.json({ data: dataFormatada });
 });
 
 const PORT = process.env.PORT || 3000;
