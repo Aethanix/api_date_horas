b8cb6bda866f729fa3a86ecc9210d9c55adf5bc0
Aethanix <aethanix@gmail.com>
2025-05-10 09:45:13 -0300

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: application/json');
-
-echo json_encode([
-    'data' => date('Y-m-d'),
-    'hora' => date('H:i:s'),
-    'timezone' => date_default_timezone_get()
-]);
